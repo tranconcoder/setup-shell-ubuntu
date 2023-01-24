c59f1bce55228d639f9db7e40765a79446a15ce5
tranconcoder <tranconcoder@gmail.com>
2023-01-24 19:50:41 +0700

    merge setup neovim into setup ubuntu

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -60,3 +60,27 @@ cp -a $KORA_ICON_PATH/kora-light ~/.local/share/icons
 cp -a $KORA_ICON_PATH/kora-light-panel ~/.local/share/icons
 cp -a $KORA_ICON_PATH/kora-pgrey ~/.local/share/icons
 
+# setup neovim
+sudo apt update
+sudo apt install neovim python3-neovim software-properties-common -y
+sudo apt-get install python3-dev python3-pip
+pip install py2neo
+npm install -g yarn
+
+# vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir -p ~/.config
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/coc
+git clone https://github.com/tranconcoder/neovim.git ~/.config/nvim
+git clone https://github.com/tranconcoder/nvim-coc.git ~/.config/coc
+
+# setup for c++
+sudo apt update
+sudo apt install ccls
+
+# setup for airline font
+mkdir -p $ROOT_PATH/fonts
+git clone https://github.com/powerline/fonts.git $ROOT_PATH/fonts/powerline-fonts
+$ROOT_PATH/fonts/powerline-fonts/install.sh
