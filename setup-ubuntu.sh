a7c8c8bef0188be7c74133ee38ec196c194499d3
tranconcoder <tranconcoder@gmail.com>
2023-01-24 20:57:25 +0700

    update

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -3,10 +3,14 @@ ROOT_PATH="$HOME/Downloads/setup"
 # create folder to setup
 mkdir -p $ROOT_PATH
 
-# install nerd font
-mkdir -p $ROOT_PATH/fonts
-wget -O  $ROOT_PATH/fonts/cascadia-code-nerdfont.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.2/CascadiaCode.zip
-unzip $ROOT_PATH/fonts/cascadia-code-nerdfont.zip -d ~/.local/share/fonts
+#################################################
+# 												#
+# 					SETUP SYSTEM 				#
+# 												#
+#################################################
+
+# install vietnamese unikey
+sudo apt -y install $(check-language-support -l fr)
 
 # install unzip package
 sudo apt-get install unzip
@@ -30,6 +34,18 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source ~/.bashrc
 nvm list-remote
 echo "run: \"nvm install <version>\" to install nodejs"
+
+
+#################################################
+# 												#
+# 					SETUP STYLES 				#
+# 												#
+#################################################
+
+# install nerd font
+mkdir -p $ROOT_PATH/fonts
+wget -O  $ROOT_PATH/fonts/cascadia-code-nerdfont.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.2/CascadiaCode.zip
+unzip $ROOT_PATH/fonts/cascadia-code-nerdfont.zip -d ~/.local/share/fonts
 
 # install gnome-tweak, gnome-extension-manager
 sudo apt update
@@ -60,14 +76,19 @@ cp -a $KORA_ICON_PATH/kora-light ~/.local/share/icons
 cp -a $KORA_ICON_PATH/kora-light-panel ~/.local/share/icons
 cp -a $KORA_ICON_PATH/kora-pgrey ~/.local/share/icons
 
-# setup neovim
+#################################################
+# 												#
+# 				    SETUP NEOVIM				#
+# 												#
+#################################################
+
 sudo apt update
 sudo apt install neovim python3-neovim software-properties-common -y
 sudo apt-get install python3-dev python3-pip
 pip install py2neo
 npm install -g yarn
 
-# vim plug
+# install neovim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config
@@ -75,6 +96,12 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.config/coc
 git clone https://github.com/tranconcoder/neovim.git ~/.config/nvim
 git clone https://github.com/tranconcoder/nvim-coc.git ~/.config/coc
+
+# install all plugin
+nvim --headless +PlugInstall +qa
+
+# update clangd plugin
+nvim --headless +CocCommand clangd.install +qa
 
 # setup for c++
 sudo apt update
@@ -84,3 +111,17 @@ sudo apt install ccls
 mkdir -p $ROOT_PATH/fonts
 git clone https://github.com/powerline/fonts.git $ROOT_PATH/fonts/powerline-fonts
 $ROOT_PATH/fonts/powerline-fonts/install.sh
+
+#################################################
+# 												#
+# 				INSTALL APPS 					#
+# 												#
+#################################################
+
+# Visual studio code
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
