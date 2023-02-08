10660aacd584b8f23cfead43a904e68c78432bc1
tvconss <tranconcoder@gmail.com>
2023-02-08 17:11:05 +0700

    update

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -99,7 +99,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
-gh auth login
+(cd ~ && gh auth login)
 
 # nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh
